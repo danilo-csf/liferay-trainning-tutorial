86ea59263ace730f24557ec8e608208d5257207a
Danilo Ferreira <danilo.ferreira@gs.liferay.com>
2018-07-13 13:17:22 -0300

    ASSET

---
+++ modules/guestbook/guestbook-api/src/main/java/com/liferay/docs/guestbook/portlet/constants/GuestbookPortletKeys.java
@@ -0,0 +1,14 @@
+package com.liferay.docs.guestbook.portlet.constants;
+
+/**
+ * @author danilo
+ */
+public class GuestbookPortletKeys {
+
+	public static final String GUESTBOOK = "com_liferay_docs_guestbook_portlet_Guestbook";
+	public static final String GUESTBOOK_ADMIN =
+			  "com_liferay_docs_guestbook_portlet_GuestbookAdminPortlet";
+	public static final String GUESTBOOK_ENTRIES_KEY ="guestbook-entries";
+	
+
+}
--- modules/guestbook/guestbook-service/src/main/java/com/liferay/docs/guestbook/service/impl/EntryLocalServiceImpl.java
+++ modules/guestbook/guestbook-service/src/main/java/com/liferay/docs/guestbook/service/impl/EntryLocalServiceImpl.java
@@ -17,6 +17,10 @@ package com.liferay.docs.guestbook.service.impl;
 import java.util.Date;
 import java.util.List;
 
+import com.liferay.asset.kernel.model.AssetEntry;
+import com.liferay.asset.kernel.model.AssetLinkConstants;
+import com.liferay.asset.kernel.service.AssetEntryLocalService;
+import com.liferay.asset.kernel.service.AssetLinkLocalService;
 import com.liferay.docs.guestbook.exception.EntryEmailException;
 import com.liferay.docs.guestbook.exception.EntryMessageException;
 import com.liferay.docs.guestbook.exception.EntryNameException;
@@ -29,6 +33,7 @@ import com.liferay.portal.kernel.model.User;
 import com.liferay.portal.kernel.search.Indexable;
 import com.liferay.portal.kernel.search.IndexableType;
 import com.liferay.portal.kernel.service.ServiceContext;
+import com.liferay.portal.kernel.util.ContentTypes;
 import com.liferay.portal.kernel.util.OrderByComparator;
 import com.liferay.portal.kernel.util.Validator;
 
@@ -90,6 +95,18 @@ public class EntryLocalServiceImpl extends EntryLocalServiceBaseImpl {
 		
 		resourceLocalService.addResources(user.getCompanyId(), groupId, userId,
 			    Entry.class.getName(), entryId, false, true, true);
+		
+		AssetEntry assetEntry = assetEntryLocalService.updateEntry(userId,
+                groupId, entry.getCreateDate(), entry.getModifiedDate(),
+                Entry.class.getName(), entryId, entry.getUuid(), 0,
+                serviceContext.getAssetCategoryIds(),
+                serviceContext.getAssetTagNames(), true, true, null, null, null, null,
+                ContentTypes.TEXT_HTML, entry.getMessage(), null, null, null,
+                null, 0, 0, null);
+
+		assetLinkLocalService.updateLinks(userId, assetEntry.getEntryId(),
+                serviceContext.getAssetLinkEntryIds(),
+                AssetLinkConstants.TYPE_RELATED);
 
 		return entry;
 	}
@@ -103,6 +120,14 @@ public class EntryLocalServiceImpl extends EntryLocalServiceBaseImpl {
 		resourceLocalService.deleteResource(
 	               serviceContext.getCompanyId(), Entry.class.getName(),
 	               ResourceConstants.SCOPE_INDIVIDUAL, entryId);
+		
+		AssetEntry assetEntry = assetEntryLocalService.fetchEntry(
+                Entry.class.getName(), entryId);
+
+		assetLinkLocalService.deleteLinks(assetEntry.getEntryId());
+
+		assetEntryLocalService.deleteEntry(assetEntry);
+		
 		return entry;
 	}
 	
@@ -154,6 +179,21 @@ public class EntryLocalServiceImpl extends EntryLocalServiceBaseImpl {
 			      user.getCompanyId(), serviceContext.getScopeGroupId(), 
 			      Entry.class.getName(), entryId, serviceContext.getGroupPermissions(),
 			      serviceContext.getGuestPermissions());
+		
+		 AssetEntry assetEntry = assetEntryLocalService.updateEntry(userId,
+                 serviceContext.getScopeGroupId(),
+                 entry.getCreateDate(), entry.getModifiedDate(),
+                 Entry.class.getName(), entryId, entry.getUuid(),
+                 0, serviceContext.getAssetCategoryIds(),
+                 serviceContext.getAssetTagNames(), true, true,
+                 entry.getCreateDate(), null, null, null,
+                 ContentTypes.TEXT_HTML, entry.getMessage(), null,
+                 null, null, null, 0, 0,
+                 serviceContext.getAssetPriority());
+
+		 assetLinkLocalService.updateLinks(userId, assetEntry.getEntryId(),
+                 serviceContext.getAssetLinkEntryIds(),
+                 AssetLinkConstants.TYPE_RELATED);
 
 		return entry;
 	}
